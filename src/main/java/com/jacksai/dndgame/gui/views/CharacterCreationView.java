e225c86ab741a3f463dc172b4dc246cc0e2d8b2b
Tomasz Bawor <tmkbwr@gmail.com>
2015-10-12 23:22:27 +0200

    Modifing charactor creation panel

--- src/main/java/com/jacksai/dndgame/gui/views/CharacterCreationView.java
+++ src/main/java/com/jacksai/dndgame/gui/views/CharacterCreationView.java
@@ -5,6 +5,7 @@ import com.googlecode.lanterna.gui.Window;
 import com.googlecode.lanterna.gui.component.Button;
 import com.googlecode.lanterna.gui.component.Label;
 import com.googlecode.lanterna.gui.component.Panel;
+import com.googlecode.lanterna.gui.component.TextBox;
 
 public class CharacterCreationView extends Window {
 
@@ -13,10 +14,49 @@ public class CharacterCreationView extends Window {
     public CharacterCreationView() {
         super(WINDOW_TITLE);
 
-        Panel characterCreationPanel = new Panel(new Border.Bevel(true), Panel.Orientation.HORISONTAL );
-        characterCreationPanel.addComponent(new Label("Create Your Character"));
-        characterCreationPanel.addComponent(new Button("EXIT", () -> System.exit(0)));
+        Panel panel = new Panel(new Border.Bevel(true), Panel.Orientation.VERTICAL );
 
-        addComponent(characterCreationPanel);
+        panel.addComponent(new Label("Create Your Character"));
+        panel.addComponent(createPanelWithCharactername());
+        panel.addComponent(createTableWithStats());
+        panel.addComponent(new Button("EXIT", () -> System.exit(0)));
+
+        addComponent(panel);
+    }
+
+    private Panel createPanelWithCharactername() {
+        Panel panel = new Panel(new Border.Standard(),Panel.Orientation.HORISONTAL);
+
+        panel.addComponent(new Label("Name: "));
+        panel.addComponent(new TextBox());
+
+        return panel;
+    }
+
+    private Panel createTableWithStats() {
+        Panel panel = new Panel(new Border.Standard(), Panel.Orientation.VERTICAL);
+
+        panel.addComponent(createStrenghtLine());
+        panel.addComponent(createDexternityLine());
+
+        return panel;
+    }
+
+    private Panel createStrenghtLine() {
+        Panel panel = new Panel(new Border.Invisible(),Panel.Orientation.HORISONTAL);
+
+        panel.addComponent(new Label("Strength: "));
+        panel.addComponent(new TextBox());
+
+        return panel;
+    }
+
+    private Panel createDexternityLine() {
+        Panel panel = new Panel(new Border.Invisible(),Panel.Orientation.HORISONTAL);
+
+        panel.addComponent(new Label("Dexternity: "));
+        panel.addComponent(new TextBox());
+
+        return panel;
     }
 }
